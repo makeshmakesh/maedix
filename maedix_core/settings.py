3985c16419b24c03f366b560922829660a48e172
makeshmakesh <makesh@lendsmart.ai>
2025-11-04 00:26:35 +0530

    fix migrate error

--- maedix_core/settings.py
+++ maedix_core/settings.py
@@ -14,11 +14,10 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-
-load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+load_dotenv()
 AUTH_USER_MODEL = 'users.CustomUser'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
