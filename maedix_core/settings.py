50eed8537c01009ef51209650075747cb82ba4b6
makeshmakesh <makesh@lendsmart.ai>
2025-11-10 21:46:57 +0530

    add static files

--- maedix_core/settings.py
+++ maedix_core/settings.py
@@ -130,15 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # optional, for extra static files
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-AWS_STORAGE_BUCKET_NAME = "maedix"
-AWS_S3_REGION_NAME = "us-east-1"  # change if needed
-AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
